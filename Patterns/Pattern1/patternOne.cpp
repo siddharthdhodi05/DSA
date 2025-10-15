7291c3ec253c1123d6cf4208c958aa3e4912a9cf
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2025-10-15 18:42:11 +0530

    Pattern-One solved

--- Patterns/Pattern1/patternOne.cpp
+++ Patterns/Pattern1/patternOne.cpp
@@ -1,6 +1,20 @@
-#include<bits/stdc++.h>
+//  Q Print the following pattern:
+
+//   * * * *
+//   * * * *
+//   * * * *
+//   * * * *
+
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-  cout<<"demo3";
+  cout<<"Please enter the size of pattern in terms of row and columns"<<endl;
+  int n; 
+  cin>>n;
+  for (int i = 0; i < n; i++)
+  {
+    for(int j=0; j<n; j++) cout<<"* ";
+    cout<<endl;
+  } 
 }
